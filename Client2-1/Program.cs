f833a3993b14a2a241df91538931b864b157234f
WhiskeyAcc <fpteam.ololo@yandex.ru>
2021-02-05 00:13:55 +0300

    1
    1

---
+++ Client2-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Lab_2
+{
+    class Program
+    {
+        private const int ECHO_PORT = 8080;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваше имя пользователя?");
+            string userName = Console.ReadLine();
+            try
+            {
+                TcpClient eClient = new TcpClient("127.0.0.1", ECHO_PORT);
+                StreamReader readerStream = new StreamReader(eClient.GetStream());
+                NetworkStream writerStream = eClient.GetStream();
+                string dataToSend = userName;
+                dataToSend += "\r\n";
+                byte[] data = Encoding.ASCII.GetBytes(dataToSend);
+                writerStream.Write(data, 0, data.Length);
+
+                while (true)
+                {
+                    Console.WriteLine(userName + ":");
+                    dataToSend = Console.ReadLine();
+                    dataToSend += "\r\n";
+                    data = Encoding.ASCII.GetBytes(dataToSend);
+                    writerStream.Write(data, 0, data.Length);
+                    if (dataToSend.IndexOf("QUIT") > -1) break;
+                    string returnData = readerStream.ReadLine();
+                    Console.WriteLine("сервер: " + returnData);
+                }
+
+                eClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Server-2-1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server_2_1
+{
+    public class ClientHandler
+    {
+        public TcpClient clientSocket;
+
+        public void RunClient()
+        {
+            StreamReader readStream = new StreamReader(clientSocket.GetStream());
+            NetworkStream writeStream = clientSocket.GetStream();
+            string returnData = readStream.ReadLine();
+            string username = returnData;
+            Console.WriteLine("Добро пожаловать на сервер " + username);
+            while (true)
+            {
+                returnData = readStream.ReadLine();
+                if (returnData.IndexOf("QUIT") > -1)
+                {
+                    Console.WriteLine("До свидания " + username);
+                    break;
+                }
+
+                Console.WriteLine(username + ": " + returnData);
+                returnData += "\r\n";
+                byte[] dataWrite = Encoding.ASCII.GetBytes(returnData);
+                writeStream.Write(dataWrite, 0, dataWrite.Length);
+            }
+
+            clientSocket.Close();
+        }
+
+    }
+
+    class Program
+    {
+        private const int ECHO_PORT = 8080;
+        public static int nClients = 0;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener clientListener = new TcpListener(ECHO_PORT);
+                clientListener.Start();
+                Console.WriteLine("Ожидание соединения...");
+                while (true)
+                {
+                    TcpClient client = clientListener.AcceptTcpClient();
+                    ClientHandler cHandler = new ClientHandler();
+                    cHandler.clientSocket = client;
+                    Thread clientThread = new Thread(new ThreadStart(cHandler.RunClient));
+                    clientThread.Start();
+                }
+
+                clientListener.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
